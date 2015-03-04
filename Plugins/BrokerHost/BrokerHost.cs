164a4e7e0114557ca1939734e114d9f5a3acffc1
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-03-04 13:43:50 +0100

    Added BrokerHost Singleton that waits for WorldServers to connect and distributes clients to these

---
+++ Plugins/BrokerHost/BrokerHost.cs
@@ -0,0 +1,64 @@
+﻿using ClientManagerPlugin;
+using KIARAPlugin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BrokerHostPlugin
+{
+    public class BrokerHost
+    {
+        public static BrokerHost Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private BrokerHost()
+        {
+            RegisterClientFunctions();
+        }
+
+        private static readonly BrokerHost instance = new BrokerHost();
+
+        private void RegisterClientFunctions()
+        {
+            string idlContents = File.ReadAllText("brokerhost.kiara");
+            KIARAServerManager.Instance.KiaraServer.AmendIDL(idlContents);
+            ClientManager.Instance.RegisterClientService("broker", false, new Dictionary<string, Delegate>{
+                {"registerAsWorldServer", (Action<string>)RegisterAsWorldServer},
+                {"clientConnected", (Action<string>)ClientConnectedToWorld},
+                {"clientDisconnected", (Action<string>)ClientDisconnectedFromWorld},
+                {"getLeastBusyServer", (Func<string>)GetLeastBusyServer}
+            });
+        }
+
+        private void RegisterAsWorldServer(string uri)
+        {
+            Servers.Add(new WorldServer { Uri = uri, ConnectedClients = 0 });
+        }
+
+        private void ClientConnectedToWorld(string uri)
+        {
+            Servers.Find(server => server.Uri == uri).ConnectedClients ++;
+        }
+
+        private void ClientDisconnectedFromWorld(string uri)
+        {
+            Servers.Find(server => server.Uri == uri).ConnectedClients --;
+        }
+
+        private string GetLeastBusyServer()
+        {
+            return Servers.OrderBy(server => server.ConnectedClients).First().Uri;
+        }
+
+        private List<WorldServer> Servers = new List<WorldServer>();
+    }
+}
