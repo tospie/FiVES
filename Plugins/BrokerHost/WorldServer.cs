6b17f054a98be7ca6468c40ff67fb2b30b73dfe7
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-03-04 13:44:00 +0100

    World Server Class

---
+++ Plugins/BrokerHost/WorldServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrokerHostPlugin
+{
+    public class WorldServer
+    {
+        public string Uri { get; set; }
+        public int ConnectedClients { get; set; }
+        public bool HasCapacity { get; set; }
+    }
+}
