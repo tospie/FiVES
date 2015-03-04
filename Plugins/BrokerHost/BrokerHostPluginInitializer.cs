e7cad84f9d5d376c933dd44edf5e42416d3ce21d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-03-04 11:54:20 +0100

    Fixed Project Settings for Broker Host Plugin

--- Plugins/BrokerHost/BrokerHostPluginInitializer.cs
+++ Plugins/BrokerHost/BrokerHostPluginInitializer.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BrokerHost
+namespace BrokerHostPlugin
 {
     public class BrokerHostPluginInitializer : IPluginInitializer
     {
